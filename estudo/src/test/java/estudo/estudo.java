fc378eb702568a5c0028f43091b2e59b56be8cdd
Rafael Suzano <rafaelsuzano@hotmail.com>
2019-12-27 00:59:37 -0300

    versao 1.0

--- estudo/src/test/java/estudo/estudo.java
+++ estudo/src/test/java/estudo/estudo.java
@@ -1,8 +1,33 @@
 package estudo;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Before;
+
 public class estudo {
 
-	public static void main(String[] args) {
-        System.out.println("Rafael");
-  }
+	public static void main(String[] args) throws IOException {
+		System.out.println("Rafael");
+		Teste();
+	}
+
+	public static Properties getProp() throws IOException {
+		Properties props = new Properties();
+		FileInputStream file = new FileInputStream("./properties/config.properties");
+		props.load(file);
+		return props;
+
+	}
+
+	@Before
+	public static void Teste() throws IOException {
+		String Caminho;
+		Properties prop = getProp();
+		Caminho = prop.getProperty("caminho");
+		System.setProperty("webdriver.chrome.drive", Caminho);
+		System.out.println(Caminho);
+	}
+
 }
