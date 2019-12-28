a63daf524fe73af93645ddc82a45000d23cd08ce
Rafael Suzano <rafaelsuzano@hotmail.com>
2019-12-27 23:38:00 -0300

    metodo print screen e print screenfull

--- estudo/src/test/java/estudo/estudo.java
+++ estudo/src/test/java/estudo/estudo.java
@@ -1,33 +1,103 @@
 package estudo;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+ 
+import org.openqa.selenium.OutputType;
 
 public class estudo {
+	private static WebDriver driver;
+	public static String Caminho;
 
+	
 	public static void main(String[] args) throws IOException {
 		System.out.println("Rafael");
-		Teste();
+		
 	}
+	@BeforeClass
+	public static void setUpTest() throws IOException  {
 
+		Properties prop = getProp();
+		Caminho = prop.getProperty("caminho");
+	
+		System.setProperty("webdriver.chrome.driver", Caminho);
+	}
+	
+	
+	
 	public static Properties getProp() throws IOException {
 		Properties props = new Properties();
 		FileInputStream file = new FileInputStream("./properties/config.properties");
 		props.load(file);
 		return props;
 
+		
 	}
 
-	@Before
-	public static void Teste() throws IOException {
-		String Caminho;
-		Properties prop = getProp();
-		Caminho = prop.getProperty("caminho");
-		System.setProperty("webdriver.chrome.drive", Caminho);
-		System.out.println(Caminho);
+	
+	public void printScreen() {
+
+		  File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		  try {
+		    // now copy the  screenshot to desired location using copyFile method
+		   
+		   FileUtils.copyFile(src, new File("C:/temp/Evidencia"+System.currentTimeMillis()+".png"));
+		         }
+		   
+		  catch (IOException e)
+		   
+		  {
+		   
+		  System.out.println(e.getMessage());
+		   
+		      }
+		   
+		  }
+
+	
+	public void printScreen2() throws IOException {
+	
+		Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+		ImageIO.write(screenshot.getImage(), "jpg", new File("C:\\temp\\fullimage.jpg"));
+
+	}		   
+		  
+
+	
+
+	@Test
+	public  void Executa() throws InterruptedException, IOException {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("http://www.uol.com/");
+//		printScreen();
+		printScreen2();
+		Thread.sleep(5000); // Let the user actually see something!
+		driver.close();
 	}
 
 }
